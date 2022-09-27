d3e1c7081ed56da92260689de7e3cc2fdccf3053
kengeene <eugene@sendyit.com>
2022-09-27 13:05:32 +0300

    Add MUI UI library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import MainApp from './components/MainApp';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 function App() {
   return (
     <div className="App">
